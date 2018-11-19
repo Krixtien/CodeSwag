4d5f2f8be3e1e29fb47146d50f1517c0c9a7c894
krixtien <comfirm247@gmail.com>
2018-11-19 22:40:29 +0100

    our images are nicely populated in our Adapter View but in an expensive way. so the next commit would be to refactor but i'm gitting this anyway to avoid stories that touches the epiglotis.

--- app/src/main/java/com/example/nwaka/codeswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/nwaka/codeswag/Controller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.nwaka.codeswag.Controller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import com.example.nwaka.codeswag.Adapters.CategoryAdapter
 import com.example.nwaka.codeswag.Model.Category
 import com.example.nwaka.codeswag.R
 import com.example.nwaka.codeswag.Services.DataService
@@ -10,15 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: ArrayAdapter<Category>
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = ArrayAdapter(this,
-            android.R.layout.simple_list_item_1,
-            DataService.categories)
+        adapter = CategoryAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
     }
 }
